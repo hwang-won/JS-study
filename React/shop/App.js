0db8f0ef7fca3f478d3055f2e697a17391276149
groot <99851831+hwang-won@users.noreply.github.com>
2022-06-12 06:24:03 +0900

    Add files via upload

--- React/shop/App.js
+++ React/shop/App.js
@@ -21,17 +21,17 @@ function App(){
       <div className="main_goods">
         <div className="main_goods_things">
           <div className="main_goods_thing">
-            <img src="https://www.yanmar.com/ltc/kr/agri/img/d3da6c6016/img_index_01.jpg" width="80%"></img>
+            <img src="https://www.yanmar.com/ltc/kr/agri/img/d3da6c6016/img_index_01.jpg" width="70%"></img>
             <h4>트렉터</h4>
             <p>상품설명</p>
           </div>
           <div className="main_goods_thing">
-            <img src="https://www.yanmar.com/ltc/kr/agri/img/6e1eb89142/img_index_19.jpg" width="80%"></img>
+            <img src="https://www.yanmar.com/ltc/kr/agri/img/6e1eb89142/img_index_19.jpg" width="70%"></img>
             <h4>콤바인</h4>
             <p>상품설명</p>
           </div>
           <div className="main_goods_thing">
-            <img src="https://www.yanmar.com/ltc/kr/agri/img/d6e76e72da/img_index_27.jpg" width="80%"></img>
+            <img src="https://www.yanmar.com/ltc/kr/agri/img/d6e76e72da/img_index_27.jpg" width="70%"></img>
             <h4>이양기</h4>
             <p>상품설명</p>
           </div>
