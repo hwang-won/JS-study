6e604b036ba9997b529c278cd17c6e3f564182d4
groot <99851831+hwang-won@users.noreply.github.com>
2022-06-12 07:40:49 +0900

    machinedata
    machinedata

---
+++ React/shop/machinedata.js
@@ -0,0 +1,28 @@
+let machinedata = [
+    {
+        id : 0,
+        title : "트렉터",
+        content : "Made in Korea",
+        price : 1200000,
+        img : "https://www.yanmar.com/ltc/kr/agri/img/d3da6c6016/img_index_01.jpg",
+        code : "d3da6c6016/img_index_01"
+    },
+    {
+        id : 1,
+        title : "콤바인",
+        content : "Made in Korea",
+        price : 1100000,
+        img : "https://www.yanmar.com/ltc/kr/agri/img/d3da6c6016/img_index_19.jpg",
+        code : "6e1eb89142/img_index_19"
+    },
+    {
+        id : 2,
+        title : "이양기",
+        content : "Made in Korea",
+        price : 1300000,
+        img : "https://www.yanmar.com/ltc/kr/agri/img/d3da6c6016/img_index_27.jpg",
+        code : "d6e76e72da/img_index_27"
+    }
+] 
+
+export default machinedata;
