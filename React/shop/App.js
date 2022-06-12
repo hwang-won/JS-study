f434af7aceb4e89ad560c13aca7159d71b7012e8
groot <99851831+hwang-won@users.noreply.github.com>
2022-06-12 09:41:25 +0900

    App.js
    상품 상세 페이지, 회사 정보 페이지, 이벤트 페이지, 404 에러 페이지 추가

--- React/shop/App.js
+++ React/shop/App.js
@@ -1,60 +1,88 @@
 import './App.css';
+// 리엑트 부트스트랩
 import { Navbar, Container, Nav } from 'react-bootstrap';
+// FrontAwesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSeedling } from "@fortawesome/free-solid-svg-icons";
+import { faOutdent, faSeedling } from "@fortawesome/free-solid-svg-icons";
+// 라우팅
+import { Route, Routes, Link, useNavigate, Outlet } from 'react-router-dom';
+// code
 import { useState } from 'react';
 import data from './machinedata.js';
 
 function App(){
 
   let [machine] = useState(data);
-  console.log({machine});
+  let navigate = useNavigate();
 
   return (
-    <div>
+    <div className="App">
       <Navbar bg="light" variant="light">
         <Container>
-        <Navbar.Brand href="#home"><FontAwesomeIcon icon={ faSeedling }/> 억새풀 <FontAwesomeIcon icon="fa-solid fa-seedling" /></Navbar.Brand>
+        <Navbar.Brand onClick={()=>{ navigate('/') }}><FontAwesomeIcon icon={ faSeedling }/> 억새풀 <FontAwesomeIcon icon="fa-solid fa-seedling" /></Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#Cart">Cart</Nav.Link>
+          <Nav.Link onClick={()=>{ navigate('/') }}>임대신청</Nav.Link>
+          <Nav.Link onClick={()=>{ navigate('/') }}>커뮤니티</Nav.Link>
+          <Nav.Link onClick={()=>{ navigate('/') }}>자료실</Nav.Link>
+          <Nav.Link onClick={()=>{ navigate('/') }}>고객지원</Nav.Link>
+          <Nav.Link onClick={()=>{ navigate('/about') }}>회사소개</Nav.Link>
+          
         </Nav>
         </Container>
       </Navbar>
 
-      <div className="main_background"> </div>
+      {/* 메인 페이지 */}
+      <Routes>
+        <Route path="/" element= {
+          <>
+          <div className="main_background"></div>        
+          <div className="main_products">
+            <div className="main_products_things">
+              {
+                machine.map((a, i) => {
+                  return (
+                    <Thing machine={ machine[i] }></Thing>
+                  )
+                })
+              }
+            </div>
+          </div> 
+          </>
+        }></Route>
 
-      <div className="main_goods">
-        <div className="main_goods_things">
-          {/* <div className="main_goods_thing">
-            <img src="https://www.yanmar.com/ltc/kr/agri/img/d3da6c6016/img_index_01.jpg" width="70%"></img>
-            <h4>{ machine[0].title }</h4>
-            <p>{ machine[0].price }</p>
+        {/* 상품 상세 페이지 */}
+        <Route path="/detail" element= {
+          <>
+          <div className="main_products_things_detail">
+            <ThingDetail machine={ machine[0] }></ThingDetail>
           </div>
-          <div className="main_goods_thing">
-            <img src="https://www.yanmar.com/ltc/kr/agri/img/6e1eb89142/img_index_19.jpg" width="70%"></img>
-            <h4>{ machine[1].title }</h4>
-            <p>{ machine[1].price }</p>
-          </div>
-          <div className="main_goods_thing">
-            <img src="https://www.yanmar.com/ltc/kr/agri/img/d6e76e72da/img_index_27.jpg" width="70%"></img>
-            <h4>{ machine[2].title }</h4>
-            <p>{ machine[2].price }</p>
-          </div> */}
-          {
-            machine.map((a, i) => {
-              return (
-                <Thing machine={ machine[i] }></Thing>
-              )
-            })
-          }
-        </div>
-      </div> 
+          </>
+        }></Route>
+
+        {/* 회사 정보 페이지 */}
+        <Route path="/about" element= {
+          <>
+            <About></About>
+          </>
+        }></Route>
+
+        {/* 이벤트 페이지 */}
+        <Route path="/event" element={<EventPage/>}>
+          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}></Route>
+          <Route path="two" element={<p>생일기념 쿠폰받기</p>}></Route>
+        </Route>
+
+        {/* 404에러 페이지 */}
+        <Route path="*" element= {
+          <div> 404 page </div>
+        }></Route>
+      </Routes>
 
     </div>
   )
 }
 
+// 메인 페이지
 function Thing(props) {
   return (
     <div>
@@ -64,6 +92,41 @@ function Thing(props) {
     </div>
   )
 }
+
+// 상품 상세 페이지
+function ThingDetail(props) {
+  return (
+    <div>
+      <img src={'https://www.yanmar.com/ltc/kr/agri/img/' + props.machine.code + '.jpg'} width="70%"></img>
+      <div className="main_products_things_detail_content">
+        <h4>{ props.machine.title }</h4>
+        <p>{ props.machine.content }</p>
+        <p>{ props.machine.price }</p>
+      </div>
+    </div>
+  )
+}
+
+// 회사 정보 페이지
+function About() {
+  return (
+    <div>
+      <h4> 억새풀 뜻 </h4>
+      <h4> 위치 </h4>
+      <h4> 주소 </h4>
+    </div>
+  )
+}
+
+// 이벤트 페이지 nested routes
+function EventPage(){
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+} 
 
 export default App;
 
