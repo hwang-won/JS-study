67c4dfe05b47649de90e4af8bca97d02dfdfa25e
groot <99851831+hwang-won@users.noreply.github.com>
2022-06-13 08:59:35 +0900

    App.js 농업기계  종류 추가

--- React/shop/App.js
+++ React/shop/App.js
@@ -6,11 +6,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSeedling } from "@fortawesome/free-solid-svg-icons";
 // 라우팅
 import { Route, Routes, Link, useNavigate, Outlet } from 'react-router-dom';
+// 서버 통신 axios
+import axios from 'axios';
 // code
 import { useState } from 'react';
 import data from './machinedata.js';
 import ThingDetail from './route/thingDetail';
 import About from './route/about';
+import HostMode from './route/hostMode';
 
 function App(){
 
@@ -28,7 +31,12 @@ function App(){
           <Nav.Link onClick={()=>{ navigate('/') }}>자료실</Nav.Link>
           <Nav.Link onClick={()=>{ navigate('/') }}>고객지원</Nav.Link>
           <Nav.Link onClick={()=>{ navigate('/about') }}>회사소개</Nav.Link>
-          
+        </Nav>
+        <Nav className="me-auto">
+          <Nav.Link onClick={()=>{ navigate('/hostMode') }}>호스트 모드</Nav.Link>
+        </Nav>
+        <Nav className="me-auto">
+          <Nav.Link onClick={()=>{ navigate('/') }}>로그인</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
@@ -40,6 +48,8 @@ function App(){
           <div className="main_background"></div>        
           <div className="main_products">
             <div className="main_products_things">
+              <h4>농업기계</h4>
+              <HorizonLine></HorizonLine>
               {
                 machine.map((a, i) => { 
                   return (
@@ -68,6 +78,13 @@ function App(){
           </>
         }></Route>
 
+        {/* 호스트 모드 페이지 */}
+        <Route path="/hostMode" element= {
+          <>
+            <HostMode></HostMode>
+          </>
+        }></Route>
+
         {/* 이벤트 페이지 */}
         <Route path="/event" element={<EventPage/>}>
           <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}></Route>
@@ -88,7 +105,7 @@ function App(){
 function Thing(props) {
   return (
     <div>
-      <img src={'https://www.yanmar.com/ltc/kr/agri/img/' + props.machine.code + '.jpg'} width="70%"></img>
+      <img src={ props.machine.img } width="50%"></img>
       <h4>{ props.machine.title }</h4>
       <p> . </p>
     </div>
@@ -104,6 +121,23 @@ function EventPage(){
     </div>
   )
 } 
+
+// 수평선
+const HorizonLine = ({ text }) => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        textAlign: "center",
+        borderBottom: "1px solid #aaa",
+        lineHeight: "0.1em",
+        margin: "10px 0 20px",
+      }}
+    >
+      <span style={{ background: "#fff", padding: "" }}>{text}</span>
+    </div>
+  );
+};
 
 export default App;
 
--- React/shop/machinedata.js
+++ React/shop/machinedata.js
@@ -1,3 +1,5 @@
+// https://www.yanmar.com/kr/agri/ 이미지 사용함
+
 let machinedata = [
     {
         id : 0,
@@ -5,23 +7,67 @@ let machinedata = [
         content : "Made in Korea",
         price : 1200000,
         img : "https://www.yanmar.com/ltc/kr/agri/img/d3da6c6016/img_index_01.jpg",
-        code : "d3da6c6016/img_index_01"
     },
     {
         id : 1,
-        title : "콤바인",
+        title : "트렉터 부속기",
         content : "Made in Korea",
         price : 1100000,
-        img : "https://www.yanmar.com/ltc/kr/agri/img/d3da6c6016/img_index_19.jpg",
-        code : "6e1eb89142/img_index_19"
+        img : "https://www.yanmar.com/ltc/kr/agri/img/360101ce76/img_index_52.jpg",
     },
     {
         id : 2,
+        title : "존디어",
+        content : "Made in Korea",
+        price : 1300000,
+        img : "https://www.yanmar.com/ltc/kr/agri/img/a7a8f9241c/img_index_08.jpg",
+    },
+    {
+        id : 3,
+        title : "존디어 부속기",
+        content : "Made in Korea",
+        price : 1000000,
+        img : "https://www.yanmar.com/ltc/kr/agri/img/3f924afbef/img_index_18.jpg",
+    },
+    {
+        id : 4,
+        title : "콤바인",
+        content : "Made in Korea",
+        price : 1100000,
+        img : "https://www.yanmar.com/ltc/kr/agri/img/6e1eb89142/img_index_19.jpg",
+    },
+    {
+        id : 5,
         title : "이양기",
         content : "Made in Korea",
         price : 1300000,
-        img : "https://www.yanmar.com/ltc/kr/agri/img/d3da6c6016/img_index_27.jpg",
-        code : "d6e76e72da/img_index_27"
+        img : "https://www.yanmar.com/ltc/kr/agri/img/d6e76e72da/img_index_27.jpg",
+    },
+    {
+        id : 6,
+        title : "야채",
+        content : "Made in Korea",
+        price : 1000000,
+        img : "https://www.yanmar.com/ltc/kr/agri/img/2d7cac0737/img_index_33.jpg",
+    },
+    {
+        id : 7,
+        title : "대두관련상품",
+        content : "Made in Korea",
+        price : 1000000,
+        img : "https://www.yanmar.com/ltc/kr/agri/img/14ac9eb7dc/img_index_25.jpg",
+    },    {
+        id : 8,
+        title : "관리기",
+        content : "Made in Korea",
+        price : 1000000,
+        img : "https://www.yanmar.com/ltc/kr/agri/img/53d8c1c3a7/img_index_29.jpg",
+    },    {
+        id : 9,
+        title : "마늘관련상품",
+        content : "Made in Korea",
+        price : 1000000,
+        img : "https://www.yanmar.com/ltc/kr/agri/img/5c64c9dfdf/img_index_45.jpg",
     }
 ] 
 
