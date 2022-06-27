12ae87350a95c72e20be236e9f3d46ac98f658e6
groot <99851831+hwang-won@users.noreply.github.com>
2022-06-27 17:52:43 +0900

    UI

--- React/shop/route/thingDetail.js
+++ React/shop/route/thingDetail.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Nav } from 'react-bootstrap';
 
 // 상품 상세 페이지
 function ThingDetail(props) {
@@ -7,25 +8,69 @@ function ThingDetail(props) {
     let { id } = useParams();
     let detailThing = props.machine.find((x) => x.id == id );
     let [alert, setAlert] = useState(true);
+    let [탭, 탭변경] = useState(0);
+    let [fade1, setFade1] = useState('');
 
     useEffect(()=> {
-        setTimeout(()=>{ setAlert(false); }, 20000)
-    })
+        let a = setTimeout(()=>{ }, 2000);
+        return ()=> {
+            clearTimeout(a);
+        }
+    }, [])
+
+    useEffect(()=>{
+        setFade1('end');
+    return ()=>{
+        setFade1('');
+    }}, [])
 
     return (
-        <div>
+        <div className={'container start ' + fade1}>
             {
                 alert == true ? 
-                <div className="coupon"> 20% 쿠폰 확인 </div>
+                <div className="coupon"> 수수료 쿠폰 확인 </div>
                 : null
             }
-            <img src={'https://www.yanmar.com/ltc/kr/agri/img/' + detailThing.code + '.jpg'} width="70%"></img>
-            <div className="main_products_things_detail_content">
-                <h5>{ detailThing.title }</h5>
-                <p>{ detailThing.content }</p>
-                <p>{ detailThing.price }원</p>
-            <button className="rentBt"> 임대하기 </button>
+            <div className="row">
+                <div className="col-md-6">
+                    <img src={detailThing.img} width="100%"></img>
+                </div>
+                <div className="col-md-6 mt-4">
+                    <h4 className="pt-5">{ detailThing.title }</h4>
+                    <p>{ detailThing.content }</p>
+                    <p>{ detailThing.price }원</p>
+                    <button className="btn btn-danger"> 임대하기 </button>
+                </div>
             </div>
+            <Nav variant="tabs" defaultActiveKey="link0">
+                <Nav.Item>
+                    <Nav.Link onClick={()=>{ 탭변경(0) }} eventKey="link0">버튼0</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={()=>{ 탭변경(1) }} eventKey="link1">버튼1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={()=>{ 탭변경(2) }} eventKey="link2">버튼2</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <TabContent 탭 = {탭}></TabContent>
+        </div>
+    )
+}
+
+function TabContent({탭}){
+    let [fade, setFade] = useState('');
+
+    useEffect(()=>{
+        setTimeout(()=>{ setFade('end') }, 100);
+    return ()=>{
+        setFade('');
+    }
+    }, [탭])
+
+    return (
+        <div className={'start ' + fade}>
+            { [<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][탭] }
         </div>
     )
 }
