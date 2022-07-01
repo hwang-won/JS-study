0e089215c35ab75d4389a7de8f8e23931ac01315
groot <99851831+hwang-won@users.noreply.github.com>
2022-07-01 17:27:34 +0900

    장바구니 기능 추가
    reduxjs/toolkit

--- React/shop/store.js
+++ React/shop/store.js
@@ -1,27 +1,30 @@
 // 컴포넌트간 state 공유 편리
-import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
 let user = createSlice({
     name : 'user',
-    initialState : 'kim',
-    reducers : {
-        changeName(state){
-            return 'john ' + state
-        }
-    }
+    initialState : { name : '안녕', age : 25 },
 })
-
-export let { changeName } = user.actions 
 
 let cart = createSlice({
     name : 'cart',
     initialState : [
-        {id : 0, name : '트렉터', count : 10},
-        {id : 1, name : '트렉터 부속기', count : 43},
-        {id : 2, name : '존디어', count : 20},
-        {id : 3, name : '존디어 부속기', count : 55},
-    ]
+        {id : 0, name : '트렉터', count : 1},
+        {id : 1, name : '트렉터 부속기', count : 1},
+    ],
+    reducers : {
+        addCount(state, action){
+            let chk = state.findIndex((a)=>{ return a.id == action.payload });
+            state[chk].count++;
+            console.log(1);
+        },
+        addItem(state, action) {
+            state.push(action.payload);
+        }
+    }
 })
+
+export let { addCount, addItem } = cart.actions 
 
 export default configureStore({
     reducer: { 
