909786baf19acbe35b69c1dd687cb112842e7423
groot <w16831111@gmail.com>
2023-02-16 08:15:39 +0900

    0215

---
+++ SW_web/day16/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import { useDispatch, useSelector } from "react-redux";
+import StateView from "./StateView";
+import StateController from "./StateController";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello world</h1>
+            <StateView />
+            <StateController />
+        </div>
+    );
+}
+
+export default App;
